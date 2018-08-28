4a5d1df3b8b54104a3e389eea6a88dd20b0ad404
Cussa <cussa.mitre@gmail.com>
2018-08-28 20:46:57 +0200

    Criação da classe Exame
    A classe Exame possuí um Professor, uma lista de Perguntas e uma lista de Alunos. Ao criar a classe, informamos o Professor e as perguntas. Os Alunos são relacionados através do método InscreverAluno.

---
+++ ExemploEscola/ExemploEscola/Exame.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExemploEscola
+{
+    class Exame
+    {
+        public Professor Professor { get; set; }
+        public List<string> Perguntas { get; set; }
+        public List<Aluno> Alunos { get; set; }
+
+        public Exame(Professor professor, List<string> perguntas)
+        {
+            Professor = professor;
+            Perguntas = perguntas;
+            Alunos = new List<Aluno>();
+        }
+
+        public void InscreverAluno(Aluno aluno)
+        {
+            if (!Alunos.Contains(aluno))
+                Alunos.Add(aluno);
+        }
+    }
+}
