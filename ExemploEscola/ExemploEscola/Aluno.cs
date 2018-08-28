f236a53a66d72ef6accace865d0a616b46eb2a5b
Cussa <cussa.mitre@gmail.com>
2018-08-28 20:30:29 +0200

    Incluindo a classe Aluno
    A classe aluno neste ponto possuí propriedades que são relativas a todas as pessoas. Logo, iremos ter um refactory no futuro.

---
+++ ExemploEscola/ExemploEscola/Aluno.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ExemploEscola
+{
+    internal class Aluno
+    {
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public string Rg { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Email { get; set; }
+        public string Celular { get; set; }
+
+        public string NomeResponsavel { get; set; }
+        public string EmailResponsavel { get; set; }
+        public string CelularResponsavel { get; set; }
+
+        public string Matricula { get; set; }
+        public DateTime DataMatricula { get; set; }
+        public string Turno { get; set; }
+        public int Serie { get; set; }
+        public string Turma { get; set; }
+    }
+}
