dc53fa154610080c74fefec4c43521c949c93d5b
Cussa <cussa.mitre@gmail.com>
2018-08-28 21:16:35 +0200

    Criando a classe ExameVestibular

---
+++ ExemploEscola/ExemploEscola/ExameVestibular.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace ExemploEscola
+{
+    internal class ExameVestibular : Exame
+    {
+        public ExameVestibular(Monitor aplicador, List<string> perguntas, int tempoExecucao) : base(aplicador, perguntas, tempoExecucao) { }
+
+        public override void Inscrever(IDiscente discente)
+        {
+            if (!Discentes.Contains(discente))
+                Discentes.Add(discente);
+        }
+    }
+}
