99bef2ffd29159ad4263b4d3e3b972f3d35dc655
Cussa <cussa.mitre@gmail.com>
2018-08-28 20:32:41 +0200

    Incluindo a classe Professor

---
+++ ExemploEscola/ExemploEscola/Professor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ExemploEscola
+{
+    internal class Professor
+    {
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public string Rg { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Email { get; set; }
+        public string Celular { get; set; }
+
+        public string Escolaridade { get; set; }
+        public string Curso { get; set; }
+        public string NumeroDiploma { get; set; }
+    }
+}
